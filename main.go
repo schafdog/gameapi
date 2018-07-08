5ed68d5b99de840b4cb3ad0bb733eca6765650c3
Dennis Schafroth <dif@topdanmark.dk>
2018-07-08 16:20:28 +0200

    Add delete of user

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"github.com/gorilla/mux"
 	"github.com/schafdog/gameapi/cassandra"
 	"github.com/schafdog/gameapi/user"
@@ -9,31 +8,22 @@ import (
 	"net/http"
 )
 
-type userCreateResponse struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
-}
-
-func createUser(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(userCreateResponse{ID: "UUID", Name: "John"})
-}
-
 func main() {
 	CassandraSession := cassandra.Session
 	defer CassandraSession.Close()
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/user", User.PostUser).Methods("POST")
 	router.HandleFunc("/user", User.GetUsers).Methods("GET")
+	router.HandleFunc("/user/{userid}", User.DeleteUser).Methods("DELETE")
 	router.HandleFunc("/user/{userid}/state", User.GetStat).Methods("GET")
 	router.HandleFunc("/user/{userid}/state", User.PutStat).Methods("PUT")
 	router.HandleFunc("/user/{userid}/friends", User.GetFriends).Methods("GET")
 	router.HandleFunc("/user/{userid}/friends", User.PutFriends).Methods("PUT")
 	/*
-		router.HandleFunc("/user/{userid}", User.Delete).Methods("DELETE")
-		   router.HandleFunc("/user/{userid}", User.Get).Methods("GET")
-		   router.HandleFunc("/user/{userid}", User.Put).Methods("PUT")
-		   router.HandleFunc("/user/{userid}", User.Patch).Methods("PATCH")
-		   router.HandleFunc("/user/{UUID}", User.Patch).Methods("HIGHSCORE")
+	   router.HandleFunc("/user/{userid}", User.Get).Methods("GET")
+	   router.HandleFunc("/user/{userid}", User.Put).Methods("PUT")
+	   router.HandleFunc("/user/{userid}", User.Patch).Methods("PATCH")
+	   router.HandleFunc("/user/{UUID}", User.Patch).Methods("HIGHSCORE")
 	*/
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
