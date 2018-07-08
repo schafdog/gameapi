ce94229a20f11d27af773b509686910c30214455
Dennis Schafroth <dif@topdanmark.dk>
2018-07-08 16:23:20 +0200

    Move to user

--- user/structs.go
+++ user/structs.go
@@ -4,7 +4,7 @@ import (
 	"github.com/gocql/gocql"
 )
 
-// User struct to hold profile data for our user
+// Model to hold profile data for our user
 type User struct {
 	Id          gocql.UUID   `json:"id"`
 	Name        string       `json:"name"`
@@ -24,17 +24,6 @@ type Stat struct {
 type UserStat struct {
 	GamesPlayed int `json:"gamesPlayed"`
 	Highscore   int `json:"score"`
-}
-
-// UsersResponse to form payload of an array of User structs
-type UsersResponse struct {
-	Users []NewUserResponse `json:"users"`
-}
-
-// NewUserResponse builds a payload of new user resource ID
-type NewUserResponse struct {
-	Id   gocql.UUID `json:"id"`
-	Name string     `json:"name"`
 }
 
 // ErrorResponse returns an array of error strings if appropriate
