03d81193d595f741c1fec1b602247d12a5cca5c2
Dennis Schafroth <dif@topdanmark.dk>
2018-07-08 23:38:00 +0200

    Optional id. Renaming

--- user/structs.go
+++ user/structs.go
@@ -6,22 +6,22 @@ import (
 
 // Model to hold profile data for our user
 type User struct {
-	Id          gocql.UUID   `json:"id"`
+	Id          *gocql.UUID  `json:"id"`
 	Name        string       `json:"name"`
 	Highscore   int          `json:"score"`
 	GamesPlayed int          `json:"gamesPlayed"`
 	Friends     []gocql.UUID `json:"friends"`
 }
 
-// UserStat to form payload returning a single User struct
-type Stat struct {
+// State to form payload returning a single User state
+type State struct {
 	Id          gocql.UUID `json:"id"`
 	Highscore   int        `json:"score"`
 	GamesPlayed int        `json:"gamesPlayed"`
 }
 
 // UserStat to form payload returning a single User struct
-type UserStat struct {
+type UserState struct {
 	GamesPlayed int `json:"gamesPlayed"`
 	Highscore   int `json:"score"`
 }
